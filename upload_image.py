6c1a1e096d6331864b6d8a25b1e39f776bafbaf9
lla105 <lla105@sfu.ca>
2025-02-04 23:35:55 -0800

    payment_status is now Form() instead of Query so that payment_status is a field in form data instead of being a query param in URL. Also did .lower() for payment_status.

--- upload_image.py
+++ upload_image.py
@@ -1,6 +1,6 @@
 #upload/main.py
 # From : https://tutorial101.blogspot.com/2023/02/fastapi-upload-image.html
-from fastapi import FastAPI, File, HTTPException, UploadFile, status, Query
+from fastapi import FastAPI, File, Form, HTTPException, Path, UploadFile, status, Query
 from fastapi.responses import FileResponse, StreamingResponse
 import os
 from random import randint
@@ -39,18 +39,13 @@ def update_payment_checks(payment_id, payment_status, file):
             raise HTTPException(status_code=404, 
             detail="ERROR: Payment not found/does not exist")
     # check if status is valid or not.
-    if payment_status not in ["pending", "completed", "due_now", 'overdue']: 
+    if payment_status.lower() not in ["pending", "completed", "due_now", 'overdue']: 
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
         detail="ERROR: Invalid status. Must be pending, completed, or failed")  
 
     # Ensure status is "completed" and file is uploaded
     if payment_status.lower() != "completed":
         print('just change the status')
-        # return {"message": "Payment updated successfully!", "file_id": str(file_id)}
-        # payments_collection.update_one(
-        #     {"_id": payment_id},
-        #     {"$set": {"payee_payment_status": "payment_status"}},
-        # )
         return {"message": "Payment updated successfully!", "file_id": 'None'}
 
     else: # request to complete 
@@ -61,52 +56,39 @@ def update_payment_checks(payment_id, payment_status, file):
             )
         # accept only PDF, PNG, JPG
         allowed_types = ["image/jpeg", "image/jpeg", "image/png", "application/pdf"]
-        if file.content_type not in allowed_types:
+        if file.content_type.lower() not in allowed_types:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="ERROR: Only PDF, PNG, and JPG files are allowed",
             )
+        return None
         
 @app.post("/update-payment/{payment_id}")
 async def update_payment(
-    payment_id : str,
-    payment_status: str = Query(..., description="New status of the payment"),  
+    payment_id : str = Path(..., description = "Unique payment ID"),
+    payment_status: str = Form(..., description="New status of the payment"),  
     file: UploadFile = File(None, description="Required for completing transaction")
 ):
-        update_payment_checks(payment_id, payment_status, file)
+        isValid = update_payment_checks(payment_id, payment_status, file)
+        if isValid:
+             return "is valid"
+        if not file:
+            print('No file. Just change status probably')
+            return "No file. Just change the status."
         contents = await file.read()
-        gridfs_obj.put(contents, filename=file.filename)
+        file_id = gridfs_obj.put(contents, filename=file.filename)
         print("Request to Complete: Success! (Valid file type)")
 
-        # # store file metadata in mongo
-        # file_data = {
-        #     "payment_id": payment_id,
-        #     "filename": file.filename,
-        #     "content_type": file.content_type,
-        #     "file_path": file_path,
-        # }
-        # file_id = files_collection.insert_one(file_data).inserted_id
 
         # # update payment status
-        # payments_collection.update_one(
-        #     {"_id": payment_id},
-        #     {"$set": {"status": "completed", "evidence_file_id": str(file_id)}},
-        # )
+        payments_collection.update_one(
+            {"_id": payment_id},
+            {"$set": {"status": "completed", "evidence_file_id": str(file_id)}},
+        )
+        download_url = f'http://127.0.0.1:8000/download/{file_id}'
+        return {"message" : "Payment updated successfully.", "download_url": download_url}
+        # return {"message": "Payment updated successfully~", "file_id": file.filename}
 
-        return {"message": "Payment updated successfully~", "file_id": file.filename}
-
-# @app.get("/download-file/{file_id}")
-# async def download_file(file_id: str):
-#     # Retrieve file metadata from MongoDB
-#     file_data = files_collection.find_one({"_id": payment_id})
-#     if not file_data:
-#         raise HTTPException(status_code=404, detail="File not found")
-
-#     return FileResponse(
-#         file_data["file_path"],
-#         media_type=file_data["content_type"],
-#         filename=file_data["filename"],
-#     )
 
 
 @app.get("/download/{file_id}")
